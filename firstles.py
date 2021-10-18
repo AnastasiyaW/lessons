9bb9ff4a923adee6b7930938533957696f7e456a
AnastasiyaW <92753226+AnastasiyaW@users.noreply.github.com>
2021-10-18 21:50:11 +0300

    firts

---
+++ firstles.py
@@ -0,0 +1,14 @@
+#print()
+#print("hello world")
+#name = "Алексей"
+#print("Hello " + name)
+#print(name + " " + "whats up")
+#print("сколько лет " + name)
+#Периметр, площадь и половинку площади
+hight = 3
+weight_coob = 5
+print((hight + weight_coob)*2)
+print(hight * weight_coob)
+print((hight * weight_coob)/2)
+
+
